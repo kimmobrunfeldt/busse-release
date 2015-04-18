05b992b9f7c28b763b150057e47c55a7ed89d549
Kimmo Brunfeldt <kimmobrunfeldt@gmail.com>
2015-04-18 22:07:27 +0300

    Release 27

--- bundle.js
+++ bundle.js
@@ -1,7 +1,7 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 var config = {
     updateInterval: 3 * 1000,
-    apiUrl: 'http://lissu-api.herokuapp.com',
+    apiUrl: 'http://lissu-api-backup.herokuapp.com',
     mapBoxKey: 'pk.eyJ1Ijoia2ltbW9icnVuZmVsZHQiLCJhIjoiX21FOWpGbyJ9.PeLVL2Rm1OZHJPYBM0lymA',
     mapBoxMapId: 'kimmobrunfeldt.l6efcofl',
     hereMapsAppId: 'lon2CLqSu9qYoH6SnBkQ',
